b9fec14242322fa8b81f930d68161d2692d109b6
s5818507 <anthony.alarcon@sesnetperu.com>
2019-02-06 17:58:27 -0500

    agregando clases

--- src/main/java/com/dcompras/gamarra/entity/Product.java
+++ src/main/java/com/dcompras/gamarra/entity/Product.java
@@ -1,5 +1,9 @@
 package com.dcompras.gamarra.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,110 +12,75 @@ import javax.persistence.Table;
 
 @Table(name="producto")
 @Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
 public class Product {
 
 	@Id
 	@GeneratedValue
 	@Column(name="id")
 	private int id;
-	
-	@Column(name="name")
-	private String name;
-	
-	@Column(name="isOutfit")
-	private boolean isOutfit;
-	
-	@Column(name="subNameShirt")
-	private String subNameShirt;
-	
-	@Column(name="subNamePants")
-	private String subNamePants;
-	
+
+	@Column(name="type")
+	private int type;
+
+	@Column(name="category")
+	private int category;
+
+	@Column(name="typeClothing")
+	private int typeClothing;
+
+	@Column(name="usesColor")
+	private String usesColor;
+
+	@Column(name="colorList")
+	private String colorList;
+
 	@Column(name="description")
 	private String description;
-	
-	
-	public int getId() {
-		return id;
-	}
+
+	@Column(name="discount")
+	private int discount;
+
+	@Column(name="imageList")
+	private String imageList;
+
+	@Column(name="size")
+	private String size;
+
+	@Column(name="isCart")
+	private boolean isCart;
+
+	@Column(name="isOutfit")
+	private boolean isOutfit;
+
+	@Column(name="isWishList")
+	private boolean isWishList;
+
+	@Column(name="name")
+	private String name;
+
+	@Column(name="priority")
+	private int priority;
+
+	@Column(name="promotion")
+	private boolean promotion;
+
+	@Column(name="quantity")
+	private int quantity;
+
+	@Column(name="retailPrice")
+	private double retailPrice;
+
+	@Column(name="subNamePants")
+	private String subNamePants;
+
+	@Column(name="subNameShirt")
+	private String subNameShirt;
+
+	@Column(name="wholesalePrice")
+	private double wholesalePrice;
 
 
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-
-
-	public String getName() {
-		return name;
-	}
-
-
-
-	public void setName(String name) {
-		this.name = name;
-	}
-
-
-
-	public boolean isOutfit() {
-		return isOutfit;
-	}
-
-
-
-	public void setOutfit(boolean isOutfit) {
-		this.isOutfit = isOutfit;
-	}
-
-
-
-	public String getSubNameShirt() {
-		return subNameShirt;
-	}
-
-
-
-	public void setSubNameShirt(String subNameShirt) {
-		this.subNameShirt = subNameShirt;
-	}
-
-
-
-	public String getSubNamePants() {
-		return subNamePants;
-	}
-
-
-
-	public void setSubNamePants(String subNamePants) {
-		this.subNamePants = subNamePants;
-	}
-
-
-
-	public String getDescription() {
-		return description;
-	}
-
-
-
-	public void setDescription(String description) {
-		this.description = description;
-	}
-	
-
-
-	public Product(int id, String name, boolean isOutfit, String subNameShirt, String subNamePants,
-			String description) {
-		this.id = id;
-		this.name = name;
-		this.isOutfit = isOutfit;
-		this.subNameShirt = subNameShirt;
-		this.subNamePants = subNamePants;
-		this.description = description;
-	}
-
-	public Product() {}
 }
